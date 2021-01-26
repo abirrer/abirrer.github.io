fd29e6f7eae1467dd63216e179f33ffbd53ebff7
Anna Birrer <annaliese.birrer@umusic.com>
2021-01-26 22:40:27 +0100

    Small styling updates and adds contact links

--- script.js
+++ script.js
@@ -2,15 +2,25 @@ updateBgColor();
   
 $("html").click(updateBgColor);
 
+$("a").click(function(e){
+  e.stopPropagation();
+});
+
 function updateBgColor() {
   const randomGeneratedColor = Math.floor(Math.random() * 16777215).toString(16);
   const bg_colour = "#" + ("000000" + randomGeneratedColor).slice(-6);
-  // console.log('background:', bg_colour, 'the background color is dark' tinycolor(bg_colour).isDark());
   
   document.body.style.backgroundColor = bg_colour;
+  
+  /*
+  Check if the background is too dark and,  
+  if so, change all font color to a light color.
+  */
   if (tinycolor(bg_colour).isDark()) {  
     document.body.style.color = "#D3D3D3";
+    $("a").css('color', "#D3D3D3");
   } else {
-    document.body.style.color = "#000000";
+    document.body.style.color = "#080808";
+    $("a").css('color', "#080808");
   }
 }
